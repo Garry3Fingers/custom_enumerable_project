27af87d2676f853eefb143d0c6be44c3f40168ee
Garry3Fingers <www.vlad78@yandex.ru>
2023-03-24 17:37:46 +0300

    Implement my_each

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -8,4 +8,15 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    i = 0
+    items = self
+
+    while i < items.length
+      result = yield items[i]
+      i += 1
+    end
+
+    result.nil? == false ? result : items
+  end
 end
